163a80b97658699bfe03c42e0c486fc335dbfb40
Brian Douglass <bhdouglass@gmail.com>
2015-05-24 15:38:23 -0400

    Added more config from the env

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ var config = {
     server: {
         ip: process.env.NODEJS_IP || '0.0.0.0',
         port: process.env.PORT || process.env.NODEJS_PORT || 3000,
-        session_secret: 'open-appstore',
-        host: 'http://local.open.uappexplorer.com:3000'
+        session_secret: process.env.SESSION_SECRET || 'open-appstore',
+        host: process.env.HOST || 'http://local.open.uappexplorer.com:3000'
     },
     mongo: {
         uri: process.env.MONGODB_URI || 'mongodb://localhost/',
