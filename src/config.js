4d0201b7e45279a0f8b8b62cfe3aba799a21a997
Brian Douglass <bhdouglass@gmail.com>
2015-05-24 15:22:56 -0400

    Added heroku's port var

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 var config = {
     server: {
         ip: process.env.NODEJS_IP || '0.0.0.0',
-        port: process.env.NODEJS_PORT || 3000,
+        port: process.env.PORT || process.env.NODEJS_PORT || 3000,
         session_secret: 'open-appstore',
         host: 'http://local.open.uappexplorer.com:3000'
     },
