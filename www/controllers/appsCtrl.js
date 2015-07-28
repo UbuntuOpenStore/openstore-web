693ffb5706791876cdcb6021f0e743591eb3706c
Brian Douglass <bhdouglass@gmail.com>
2015-07-27 23:32:24 -0400

    Added package contents and full manifest to app view

--- www/controllers/appsCtrl.js
+++ www/controllers/appsCtrl.js
@@ -3,6 +3,8 @@
 angular.module('openstore').controller('appsCtrl', function($scope, $state, $http) {
     $scope.apps = {};
     $scope.app = null;
+    $scope.tab = 'contents';
+
     $http.get('/api/apps').then(function(response) {
         $scope.apps = response.data.data;
 
@@ -10,8 +12,24 @@ angular.module('openstore').controller('appsCtrl', function($scope, $state, $htt
             angular.forEach($scope.apps, function(app) {
                 if (app.id == $state.params.name) {
                     $scope.app = app;
+                    $scope.tab = 'contents';
                 }
             });
         }
     });
+
+    $scope.appType = function(hook) {
+        var type = 'App';
+        if (hook.dekstop) {
+            type = 'App';
+        }
+        else if (hook.scope) {
+            type = 'Scope';
+        }
+        else if (hook['push-helper']) {
+            type = 'Push Helper';
+        }
+
+        return type;
+    };
 });
