3ba07170ca191ec1e3fa4de44b8d7ae7db4cda74
Brian Douglass <bhdouglass@gmail.com>
2016-03-24 00:36:39 -0400

    Added cors to the api

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 
 var paths = {
-    js: ['www/**/*.js', 'src/**/*.js'],
+    jshint: ['www/**/*.js', 'src/**/*.js'],
 };
 
 gulp.task('lint', function() {
-    gulp.src(paths.js)
+    gulp.src(paths.jshint)
         .pipe(jshint())
         .pipe(jshint.reporter(stylish))
         .pipe(jshint.reporter('fail'));
--- src/index.js
+++ src/index.js
@@ -21,6 +21,12 @@ else {
     var app = express();
     app.set('json spaces', 2);
 
+    app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
+    });
+
     auth.setup(app);
     apps.setup(app);
 
