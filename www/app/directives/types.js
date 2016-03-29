a8d878198a8559ad0e792475f35730e8598a3196
Brian Douglass <bhdouglass@gmail.com>
2016-03-29 00:24:19 -0400

    Added types list to the app interface

---
+++ www/app/directives/types.js
@@ -0,0 +1,20 @@
+var types = function() {
+	return {
+		restrict: 'E',
+		replace: false,
+		scope: {
+			model: '=ngModel'
+		},
+		templateUrl: '/app/partials/types.html',
+		link: function($scope) {
+			$scope.types = {
+				app: 'App',
+				scope: 'Scope',
+				webapp: 'Web App',
+				snappy: 'Snappy App',
+			};
+		}
+	};
+};
+
+module.exports = types;
