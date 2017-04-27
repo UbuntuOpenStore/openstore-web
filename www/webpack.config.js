fcecd823f9e8ed0b627cfbe9022a7c7b292e7c71
Brian Douglass <bhdouglass@gmail.com>
2017-04-27 01:14:33 -0400

    Updated to webpack 2

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,40 +1,30 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     context: __dirname,
     entry: './app.js',
     output: {
-        hash: true,
         path: __dirname,
         filename: 'bundle.js'
     },
 
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader'
-            },
-            {
+                use: ['style-loader', 'css-loader'],
+            }, {
                 test: /\.(png|gif)$/,
-                loader: 'url'
-            },
-            {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'url-loader?limit=10000&minetype=application/font-woff'
-            },
-            {
+                use: 'url-loader',
+            }, {
                 test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'file-loader'
+                use: 'file-loader'
+            }, {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: 'url-loader',
             },
-        ],
-        preLoaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'jshint-loader'
-            }
         ]
     },
 
@@ -43,12 +33,12 @@ module.exports = {
         port: 8081,
     },
     resolve: {
-        modulesDirectories: ['node_modules', 'bower_components']
+        alias: { //Aliases for bower components
+            swipebox: path.resolve(__dirname, 'bower_components/swipebox/src/js/jquery.swipebox.js'),
+            Uploadcare: path.resolve(__dirname, 'bower_components/Uploadcare/uploadcare.min.js'),
+        }
     },
     plugins: [
-        new webpack.ResolverPlugin(
-            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('.bower.json', ['main'])
-        ),
         new webpack.DefinePlugin({'process.env': {
             'API': (process.env.NODE_ENV == 'production') ? JSON.stringify('https://open.uappexplorer.com') : JSON.stringify('http://local.open.uappexplorer.com'),
             'UPLOADCARE_KEY': JSON.stringify(process.env.UPLOADCARE_KEY),
@@ -56,13 +46,8 @@ module.exports = {
         //new webpack.optimize.DedupePlugin(),
         //new webpack.optimize.UglifyJsPlugin(),
         new HtmlWebpackPlugin({
-            hash: true,
             filename: 'index.html',
             template: __dirname + '/index.html',
         }),
     ],
-
-    jshint: {
-        failOnHint: (process.env.NODE_ENV == 'production'),
-    }
 };
