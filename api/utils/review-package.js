e1fa2a0d2cae6784dbfcb5a3252694c02383c6fe
Brian Douglass <bhdouglass@gmail.com>
2017-05-04 01:14:57 -0400

    Cleaned up logging in review-package

--- api/utils/review-package.js
+++ api/utils/review-package.js
@@ -5,32 +5,39 @@ const logger = require('./logger');
 
 const exec = require('child_process').exec;
 
+//TODO return the actual problem
+function parseReview(review) {
+    let manualReview = false;
+
+    for (let key in review) {
+        for (let level in review[key]) {
+            for (let label in review[key][level]) {
+                if (review[key][level][label].manual_review) {
+                    manualReview = true;
+                }
+            }
+        }
+    }
+
+    return manualReview;
+}
+
 function reviewPackage(file) {
     return new Promise((resolve, reject) => {
         let command = `${config.clickreview.command} --json ${file}`;
         exec(command, {env: {PYTHONPATH: config.clickreview.pythonpath}}, function callback(err, stdout, stderr) {
             if (err) {
                 logger.error('Error processing package for review: ' + err);
-                logger.error(`Command: ${command}, PYTHONPATH: ${config.clickreview.pythonpath}`);
-                logger.error(stdout);
-                logger.error(stderr);
+                if (stderr) {
+                    logger.error(stderr);
+                }
+
                 resolve(true);
             }
             else {
                 let review = JSON.parse(stdout);
-                let manualReview = false;
-                for (let key in review) {
-                    for (let level in review[key]) {
-                        for (let label in review[key][level]) {
-                            if (review[key][level][label].manual_review) {
-                                manualReview = true;
-                                //TODO explain why automatic review failed
-                            }
-                        }
-                    }
-                }
 
-                resolve(manualReview);
+                resolve(parseReview(review));
             }
         });
     });
