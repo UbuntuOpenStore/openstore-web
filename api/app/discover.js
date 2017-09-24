9f3f57fbfa557382e381bf73ef455c47343ca7ce
Brian Douglass <bhdouglass@gmail.com>
2017-09-24 00:12:31 -0400

    Fixed caching for the discover page

--- api/app/discover.js
+++ api/app/discover.js
@@ -2,56 +2,84 @@
 
 const db = require('../db');
 const config = require('../utils/config');
-const discover = require('./json/discover_apps.json');
+const discoverJSON = require('./json/discover_apps.json');
 const helpers = require('../utils/helpers');
 
 const shuffle = require('shuffle-array');
+const moment = require('moment');
 
 function setup(app) {
-    discover.highlight.image = config.server.host + discover.highlight.image;
+    discoverJSON.highlight.image = config.server.host + discoverJSON.highlight.image;
 
-    let staticCategories = discover.categories.filter((category) => (category.ids.length > 0));
-    db.Package.findOne({id: discover.highlight.id}).then((highlightedApp) => {
-        discover.highlight.app = highlightedApp;
+    let discoverCache = null;
+    let discoverDate = null;
+    function getDiscover() {
+        let now = moment();
+        if (!discoverDate || now.diff(discoverDate, 'minutes') > 10 || !discoverCache) { //Cache miss
+            let discover = JSON.parse(JSON.stringify(discoverJSON));
+            let staticCategories = discover.categories.filter((category) => (category.ids.length > 0));
 
-        return Promise.all(staticCategories.map((category) => {
-            return db.Package.find({id: {$in: category.ids}});
-        }));
-    }).then((results) => {
-        staticCategories.forEach((category, index) => {
-            category.apps = results[index];
-        });
+            return Promise.all([
+                db.Package.findOne({id: discover.highlight.id}),
 
-        return db.Package.find({
-            published: true,
-            types: {
-                $in: ['app', 'webapp', 'scope', 'webapp+'],
-            },
-        }).limit(10).sort('-published_date');
-    }).then((newest) => {
-        let category = discover.categories.filter((category) => (category.name == 'New Apps'))[0];
-        category.ids = newest.map((app) => app.id);
-        category.apps = newest;
+                Promise.all(staticCategories.map((category) => {
+                    return db.Package.find({id: {$in: category.ids}});
+                })),
 
-        return db.Package.find({
-            published: true,
-            types: {
-                $in: ['app', 'webapp', 'scope', 'webapp+'],
-            },
-        }).limit(10).sort('-updated_date');
-    }).then((updated) => {
-        let category = discover.categories.filter((category) => (category.name == 'Updated Apps'))[0];
-        category.ids = updated.map((app) => app.id);
-        category.apps = updated;
-    });
+                db.Package.find({
+                    published: true,
+                    types: {
+                        $in: ['app', 'webapp', 'scope', 'webapp+'],
+                    },
+                }).limit(10).sort('-published_date'),
+
+                db.Package.find({
+                    published: true,
+                    types: {
+                        $in: ['app', 'webapp', 'scope', 'webapp+'],
+                    },
+                }).limit(10).sort('-published_date')
+            ]).then((results) => {
+                discover.highlight.app = results[0];
+
+                staticCategories.forEach((category, index) => {
+                    category.apps = results[1][index];
+                });
+
+                let newCategory = discover.categories.filter((category) => (category.name == 'New Apps'))[0];
+                newCategory.ids = results[2].map((app) => app.id);
+                newCategory.apps = results[2];
+
+                let usedCategory = discover.categories.filter((category) => (category.name == 'Updated Apps'))[0];
+                usedCategory.ids = results[3].map((app) => app.id);
+                usedCategory.apps = results[3];
+
+                staticCategories.forEach((category) => {
+                    category.ids = shuffle(category.ids);
+                    category.apps = shuffle(category.apps);
+                });
+
+                discoverCache = discover;
+                discoverDate = now;
+                return discover;
+            });
+        }
+        else { //Cache hit
+            console.log('cache hit');
+            return new Promise((resolve) => {
+                resolve(discoverCache);
+            });
+        }
+    }
+
 
     app.get(['/api/apps/discover', '/api/v1/apps/discover'], function(req, res) {
-        staticCategories.forEach((category) => {
-            category.ids = shuffle(category.ids);
-            category.apps = shuffle(category.apps);
+        getDiscover().then((discover) => {
+            helpers.success(res, discover);
+        }).catch(() => {
+            helpers.error(res, 'Unable to fetch discovery data at this time');
         });
 
-        helpers.success(res, discover);
     });
 }
 
