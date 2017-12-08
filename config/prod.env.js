d072cef56bc2ae45e314b05b23b87101fa6c208c
Brian Douglass <bhdouglass@gmail.com>
2017-12-08 13:03:10 -0500

    Use API env var when building for production

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API: JSON.stringify('https://open.uappexplorer.com'),
+  API: JSON.stringify(process.env.API || 'https://open.uappexplorer.com'),
   UPLOADCARE_KEY: JSON.stringify(process.env.UPLOADCARE_KEY),
 }
