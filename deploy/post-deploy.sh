1883c5e06959a816d36db7b1dd4384686c9ec2b6
Brian Douglass <bhdouglass@gmail.com>
2019-07-16 23:49:59 -0400

    Fix removing old version after deploying

--- deploy/post-deploy.sh
+++ deploy/post-deploy.sh
@@ -16,6 +16,7 @@ npm run build
 rm -f /srv/openstore-web/current
 ln -s /srv/openstore-web/$VERSION /srv/openstore-web/current
 
+cd /srv/openstore-web/
 echo "Going to remove old versions"
 ls -1t | grep -v current | tail -n +10
 ls -1t | grep -v current | tail -n +10 | xargs -d '\n' -r rm -r --
