4f341f762ab0461e574c8d61f3f6630fac33fa12
Brian Douglass <bhdouglass@gmail.com>
2019-11-30 00:38:31 -0500

    Added staging deploy

--- deploy/post-deploy.sh
+++ deploy/post-deploy.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 VERSION=$1
-export VUE_APP_VERSION=$VERSION
-source /srv/openstore/env.sh
+ENV=${2:-""}
+BASE=/srv/openstore-web$ENV
+
+export VUE_APP_VERSION=$VERSION$ENV
+source /srv/openstore$ENV/env.sh
 
 set -x
 set -e
 
-cd /srv/openstore-web/$VERSION
+cd $BASE/$VERSION
 # TODO use a cached node_modules
 npm install
 npm install --only=dev
 npm run build
 
-rm -f /srv/openstore-web/current
-ln -s /srv/openstore-web/$VERSION /srv/openstore-web/current
+rm -f $BASE/current
+ln -s $BASE/$VERSION $BASE/current
 
-cd /srv/openstore-web/
+cd $BASE/
 echo "Going to remove old versions"
 ls -1t | grep -v current | tail -n +10
 ls -1t | grep -v current | tail -n +10 | xargs -d '\n' -r rm -r --
